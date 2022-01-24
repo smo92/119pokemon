bc74e4f683b8c39da23a30fe133233b2891714a7
smo92 <moran.s215@gmail.com>
2022-01-24 15:41:15 -0700

    added pokelist and poketypes array

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
-let pokemonList=[
-  {name:'Charmeleon', height:6, type: 'Fire'},
-  {name:'Squirtle', height:4, type:'water'},
-  {name:'Balbasour', height:7, type:'grass,posion'}
+//IIFE
+let pokemonRepository = (function(){
 ];
+ 
+//Function to add pokemon to list
+  function add(newPokemon){
+    //validating it is an object
+        if(typeof newPokemon === 'object' && !Array.isArray(newPokemon)){
+         //validating that it use object keys expected
+          if (Object.keys(newPokemon)[0] === 'name' &&
+              Object.keys(newPokemon)[1] ==='height' && 
+              Object.keys(newPokemon)[2] === 'types'){
 
-for (let i = 0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 6){
-    document.write('<p>' + pokemonList[i].name + ' height is ' + pokemonList[i].height + ' wow that big!' + '</p>')
-  }else{
-    document.write('<p>' + pokemonList[i].name + ' height is ' + pokemonList[i].height + '</p>')
+        pokemonList.push(newPokemon);  
+      }
+    }
   }
-}
+  //adding a findPokenmon function to search by name
+  function searchPokemon(searchName){
+    let findPokemon = pokemonList.filter (pokemon => pokemon.name === searchName);
+      return findPokemon
+  }
+  //Function to show all pokemon on the list
+  function getAll(){
+    return pokemonList;
+  }
+  return{
+    add: add,
+    getAll: getAll,
+    searchPokemon: searchPokemon
+  };
+
+})();
+
+console.log(pokemonRepository.searchPokemon('Squirtle'));
+
+pokemonRepository.add({name:'Pikachu', height: '0.4', types: 'Fire'});
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write('<p>' + pokemon.name + ' is ' +    pokemon.height + ' tall, and is ' +             pokemon.types + ' type!' + '</p>');
+
+  });
