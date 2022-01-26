7bdda25dc1aa6b69bc275e80f85f373c005e3b9a
smo92 <moran.s215@gmail.com>
2022-01-26 11:17:52 -0700

    rearranging things to clean up the code a bit. misc.

--- js/script.js
+++ js/script.js
@@ -63,9 +63,9 @@ let pokemonRepository = (function(){
   return{
     add: add,
     getAll: getAll,
-    searchPokemon: searchPokemon
+    searchPokemon: searchPokemon,
+    addListItem: addListItem
   };
-
 })();
 
 //calling searchPokemon function to console
@@ -77,8 +77,6 @@ pokemonRepository.add({name:'Pikachu', height: '0.4', types: 'Fire'});
 //calling getAll function to show all pokemon in the list 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write('<p>' + pokemon.name + ' is ' +    pokemon.height + ' tall, and is ' +             pokemon.types + ' type!' + '</p>');
 //calling the function addListItem to generate buttons for each
 // pokemon on the list, that shows additonal details. As well as
 //pushing them to the HTML page
