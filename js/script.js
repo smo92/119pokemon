086220084d869d4ad445f5217606202a21a34360
smo92 <moran.s215@gmail.com>
2022-02-01 08:28:12 -0700

    deleting old functions and returns

--- js/script.js
+++ js/script.js
@@ -1,30 +1,9 @@
 //IIFE
-let pokemonRepository = (function(){
-
-  let pokemonRepo = [
-  {name:'Charmander', height:0.6, types: ['Fire']},
-  {name:'Squirtle', height:0.4, types:['water']},
-  {name:'Balbasour', height:0.7, types:['Grass,Posion']},
-  {name: 'Golbat', height:1.6, types: ['Flying','Posion']},
-  {name:'Alakazam', height:1.5, types:['physic']},
-];
-  let pokemonTypes = ['Fire','Water','Grass','Posion','Flying','Physic'
-];
 let pokemonRepository = (function () {
   let pokemonRepo = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
  
 //Function to add pokemon to list
-  function add(newPokemon){
-    //validating it is an object
-        if(typeof newPokemon === 'object' && !Array.isArray(newPokemon)){
-         //validating that it use object keys expected
-        if (Object.keys(newPokemon)[0] === 'name' &&
-            Object.keys(newPokemon)[1] ==='height' && 
-            Object.keys(newPokemon)[2] === 'types'){
-
-        pokemonRepo.push(newPokemon);  
-      }
     }
   }
   //adding a findPokenmon function to search by name
@@ -58,33 +37,13 @@ let pokemonRepository = (function () {
       showDetails(pokemon);
     });
    } 
-   // function that will give the details of the pokemon to console log 
-    function showDetails(pokemon){
-      console.log(pokemon);
   }
   // the returns for each function in the IIFE
   return{
     add: add,
     getAll: getAll,
     searchPokemon: searchPokemon,
-    addListItem: addListItem
   };
 })();
 
-//calling searchPokemon function to console
-console.log(pokemonRepository.searchPokemon('Squirtle'));
 
-//calling add function to add new pokemon to pokeRepo
-pokemonRepository.add({name:'Pikachu', height: '0.4', types: 'Fire'});
-
-//calling getAll function to show all pokemon in the list 
-console.log(pokemonRepository.getAll());
-
-//calling the function addListItem to generate buttons for each
-// pokemon on the list, that shows additonal details. As well as
-//pushing them to the HTML page
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
-
-  });
-    
